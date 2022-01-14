ee58d6621846750582c0b18427f12b19df2b354b
Stjepan Mamusa <smamusa@hotmail.com>
2022-01-14 23:57:20 +0100

    initial commit

---
+++ src/main/java/com/blackbetty/blackbettyapi/BlackbettyApiApplication.java
@@ -0,0 +1,13 @@
+package com.blackbetty.blackbettyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackbettyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackbettyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackbetty/blackbettyapi/BlackbettyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackbetty.blackbettyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackbettyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
